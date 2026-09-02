08f6f7220071416f252f1de734eff1fba8a3b1d2
Play Collector <play-collector@local>
2026-09-02 14:10:18 +0000

    Pipeline sync 2026-09-02 14:10

---
+++ chicken road 2/Chicken Road 2-Tarra-Market/agent_src/io/appmetrica/analytics/coreapi/internal/identifiers/AdTrackingInfoResult.java
@@ -0,0 +1,39 @@
+package io.appmetrica.analytics.coreapi.internal.identifiers;
+
+import android.text.TextUtils;
+import p.AbstractC0819i;
+
+/* loaded from: classes.dex */
+public class AdTrackingInfoResult {
+    public final AdTrackingInfo mAdTrackingInfo;
+    public final String mErrorExplanation;
+    public final IdentifierStatus mStatus;
+
+    public AdTrackingInfoResult() {
+        this(null, IdentifierStatus.UNKNOWN, "identifier info has never been updated");
+    }
+
+    public static AdTrackingInfoResult getProviderUnavailableResult(String str) {
+        return new AdTrackingInfoResult(null, IdentifierStatus.IDENTIFIER_PROVIDER_UNAVAILABLE, str);
+    }
+
+    public boolean isValid() {
+        AdTrackingInfo adTrackingInfo = this.mAdTrackingInfo;
+        return (adTrackingInfo == null || TextUtils.isEmpty(adTrackingInfo.advId)) ? false : true;
+    }
+
+    public String toString() {
+        StringBuilder sb = new StringBuilder("AdTrackingInfoResult{mAdTrackingInfo=");
+        sb.append(this.mAdTrackingInfo);
+        sb.append(", mStatus=");
+        sb.append(this.mStatus);
+        sb.append(", mErrorExplanation='");
+        return AbstractC0819i.b(sb, this.mErrorExplanation, "'}");
+    }
+
+    public AdTrackingInfoResult(AdTrackingInfo adTrackingInfo, IdentifierStatus identifierStatus, String str) {
+        this.mAdTrackingInfo = adTrackingInfo;
+        this.mStatus = identifierStatus;
+        this.mErrorExplanation = str;
+    }
+}
